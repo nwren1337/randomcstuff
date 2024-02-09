58019720ecc8363be095b97926cd6986f37c2a88
nwren <nwren>
2024-02-09 16:53:44 -0500

    implemented individual node removal and added tests to main

--- src/binarysearchtree/bst.c
+++ src/binarysearchtree/bst.c
@@ -26,10 +26,74 @@ btnode* insert(btnode* root, int val) {
     return root;
 }
 
+bool is_leaf(btnode* node) {
+    if(node != NULL) {
+        return (node->left == NULL) && (node->right == NULL);
+    }
+    return true;
+}
+
+bool single_child(btnode* node) {
+    if(node != NULL) {
+        return (node->left == NULL && node->right != NULL) || 
+               (node->left != NULL && node->right == NULL);
+    }
+    return false;
+}
+
+btnode* find_min(btnode* root) {
+    if(root != NULL) {
+        if (root->left != NULL) {
+            return find_min(root->left);
+        }
+    }
+    return root;
+}
+
+btnode* remove_node(btnode* root, int target) {
+    //guard against null
+    if(root != NULL) {
+        //recurse to target
+        if(target < root->val) {
+            root->left = remove_node(root->left, target);
+        } else if(target > root->val) {
+            root->right = remove_node(root->right, target);
+        } else {
+            //we are at our target
+            //if the target is a leaf remove the node and return NULL
+            if(is_leaf(root)) {
+                free(root);
+                return NULL;
+            }
+            //if the target has a single child, replace the target with the child and return it
+            else if(single_child(root)) {
+                btnode* temp;
+                if (root->left != NULL) {
+                    temp = root->left;
+                } else {
+                    temp = root->right;
+                }
+                free(root);
+                return temp;
+            }
+            //if the target has two children
+            else {
+                // find the minimum node in the right subtree
+                btnode* temp = find_min(root->right);
+                //set this node to the minimum in the right
+                root->val = temp->val;
+                //remove the duplicate from the right subtree
+                root->right = remove_node(root->right, temp->val);
+            }
+        }
+    }
+    return root;
+}
+
 void print_in_order(btnode* root) {
     if(root != NULL) {
         print_in_order(root->left);
-        printf(" %i ", root->val); 
+        printf("%i ", root->val); 
         print_in_order(root->right);
     }
 }
--- src/binarysearchtree/main.c
+++ src/binarysearchtree/main.c
@@ -2,6 +2,7 @@
 #include "bst.h"
 
 int main() {
+    /* Test insertion and printing */
     btnode* node = NULL;
     node = insert(node, 5);
     insert(node, 3);
@@ -9,7 +10,40 @@ int main() {
     insert(node, 8);
     insert(node, 10);
     print_in_order(node);
-    printf("Cool.\n");
+    printf("\n");
+
+    /* Test removing a single node */
+    btnode* node2 = NULL;
+    node2 = insert(node2, 5);
+    insert(node2, 3);
+    insert(node2, 6);
+    print_in_order(node2); // 3 5 6
+    printf("\n");
+    //remove a leaf
+    remove_node(node2, 3);
+    print_in_order(node2); // 5 6
+    printf("\n");
+    //re-add a leaf
+    insert(node2, 1);
+    print_in_order(node2); // 1 5 6
+    printf("\n");
+    //remove the root
+    remove_node(node2, 5);
+    print_in_order(node2); // 1 6
+    printf("\n");
+    //set up subtree with single child to swap
+    insert(node2, 7);
+    insert(node2, 8);
+    insert(node2, 9);
+    print_in_order(node2); // 1 6 7 8 9
+    printf("\n");
+    remove_node(node2, 8);
+    print_in_order(node2); // 1 6 7 9
+    printf("\n");
+
+
+    /* cleanup */
     free_tree(node);
+    free_tree(node2);
     return 0;
 }
