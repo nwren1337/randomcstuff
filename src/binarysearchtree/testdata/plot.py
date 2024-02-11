02b71b001087658fe5cbea4ffb72b4a0a468d863
nwren <nwren>
2024-02-10 21:16:28 -0500

    generated some plots

--- src/binarysearchtree/testdata/plot.py
+++ src/binarysearchtree/testdata/plot.py
@@ -1,6 +1,33 @@
-import matplotlib.pyplot as pyplot
+import matplotlib.pyplot as plt
 import json
+import pprint
 
-test_data = json.load(open('tests.json', r))
+test_data = json.load(open('tests.json', 'r'))
 
-json.pprint(test_data)
+test_names = [key for key, value in test_data.items()]
+
+insertion_tests = filter(lambda x: "Insertion" in x, test_names)
+searching_tests = filter(lambda x: "Searching" in x, test_names)
+
+for test in insertion_tests:
+    xAxis = [key for key, value in test_data[test].items()]
+    yAxis = [value for key, value in test_data[test].items()]
+    plt.plot(xAxis, yAxis, label=test)
+
+plt.grid(True)
+plt.xlabel('number of nodes')
+plt.ylabel('time (s)')
+plt.legend(loc='best')
+plt.show()
+
+for test in searching_tests:
+    xAxis = [key for key, value in test_data[test].items()]
+    yAxis = [value for key, value in test_data[test].items()]
+    plt.plot(xAxis, yAxis, label=test)
+
+
+plt.grid(True)
+plt.xlabel('number of nodes')
+plt.ylabel('time (s)')
+plt.legend(loc='best')
+plt.show()
